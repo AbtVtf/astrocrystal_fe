35be33198bebc31957dc04dd37fc8cea8d59692c
AbtVtf <abt.vtf@gmail.com>
2023-09-16 21:20:32 +0300

    fixes all bugs and errors

--- astrocrystals/App.tsx
+++ astrocrystals/App.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import store from './src/app/store';
 import AppNavigator from './src/navigation/AppNavigator';
-
 const App: React.FC = () => {
   return (
     <Provider store={store}>
