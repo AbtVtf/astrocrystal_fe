f1a1693d3323a7028240d0d3cb94492d9c07b082
cscoder97 <44376076+cscoder97@users.noreply.github.com>
2023-09-16 21:59:57 +0300

    push ios

--- astrocrystals/src/components/Navbar.tsx
+++ astrocrystals/src/components/Navbar.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
+import AnagramScreen from '../screens/AnagramScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -9,7 +10,7 @@ const Navbar: React.FC = () => {
   return (
     <Tab.Navigator>
       <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Page2" component={HomeScreen} />
+      <Tab.Screen name="Page2" component={AnagramScreen} />
       <Tab.Screen name="Page3" component={HomeScreen} />
       <Tab.Screen name="Page4" component={HomeScreen} />
       <Tab.Screen name="Page5" component={HomeScreen} />
